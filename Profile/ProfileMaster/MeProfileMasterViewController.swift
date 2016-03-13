78fc2e9f63ba44bb427947f265e22e43fd38dacb
Yumen <gawainus97@gmail.com>
2016-03-13 23:14:44 +0100

    updated pods

--- Profile/ProfileMaster/MeProfileMasterViewController.swift
+++ Profile/ProfileMaster/MeProfileMasterViewController.swift
@@ -170,7 +170,6 @@ class MeProfileMasterViewController: ProfileMasterViewController,
                 self.pVC.getProfileImages()
 
                 self.pPCVC.dataInit()
-
             }
         }
     }
