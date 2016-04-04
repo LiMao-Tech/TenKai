9262481077c2f1a52d5423513cad112b5191deb0
Yumen <gawainus97@gmail.com>
2016-04-04 11:06:06 +0200

    deleted redundant lines

--- Ten/AppDelegate.swift
+++ Ten/AppDelegate.swift
@@ -145,7 +145,6 @@ class AppDelegate: UIResponder,
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
         
-        LOC_MANAGER.stopUpdatingLocation()
         print("in Did Enter Background State")
     }
 
